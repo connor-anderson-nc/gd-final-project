70db5bd65d5cdec05987e0cad5609f2be193b14e
connor anderson <anderson5459@sps81.org>
2025-05-30 14:21:38 -0700

    minor changes to computer class

---
+++ scripts/types/computer.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name computer
+
+@export_category("system-info")
+@export var sys_name: String
+@export var os_name: String
+@export var storage_space: int
+@export var ram_name: String
+@export var ram_capacity: int
+@export var processor_name: String
+@export var processor_capacity: int
