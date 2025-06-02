240f87b2810b4edb05a9b3525d07ea65fbcce7a0
connor anderson <anderson5459@sps81.org>
2025-06-02 14:26:13 -0700

    finaly started file sys

--- scripts/types/computer.gd
+++ scripts/types/computer.gd
@@ -9,3 +9,26 @@ class_name computer
 @export var ram_capacity: int
 @export var processor_name: String
 @export var processor_capacity: int
+
+#########################
+####### file sys ########
+#########################
+func _ready() -> void:
+	var root: dir = dir.new(null, "root")
+
+class node:
+	var parent: dir
+	var f_name: String
+	
+	func _init(p, n) -> void:
+		parent = p
+		f_name = n
+
+class dir extends node:
+	var children = {}
+	
+	func add_child(child: node):
+		children[child.f_name] = child
+
+class file extends node:
+	var data
