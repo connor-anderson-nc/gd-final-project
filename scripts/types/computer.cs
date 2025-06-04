575f2db38247b09ba33923bdf60b161e1b548d0d
connor anderson <anderson5459@sps81.org>
2025-06-04 14:23:55 -0700

    stuff

--- scripts/types/computer.cs
+++ scripts/types/computer.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-public class Computer {
+[GlobalClass]
+public partial class Computer : Node
+{
     [ExportGroup("sys info")]
     [Export]
     string sys_name;
@@ -21,13 +23,39 @@ public class Computer {
     int processor_capacity;
 }
 
-public class FileSys {
-    int max_storage;
+public class FileSys
+{
+    const String PathStart = "#";
 
+    int max_storage;
     int used_space;
+
     Pointer active_dir;
 
+    dir root = new dir();
+
     public FileSys() { }
+
+    /////////////////////
+    /// utility funcs ///
+    /////////////////////
+
+    private Pointer ReadPath(String _Path)
+    {
+        String[] PathArr = (PathStart + _Path).Split('/');
+        Pointer current;
+        switch (PathArr[0])
+        {
+            case PathStart:
+                current = &root
+                break;
+        }
+       
+    }
+
+    ////////////////////
+    /// node classes ///
+    ////////////////////
 
     private class node
     {
@@ -35,13 +63,25 @@ public class FileSys {
         dir parent;
     }
 
-    private class dir : node {
+    private class dir : node
+    {
         Dictionary<String, node> children;
+        public dir()
+        {
+            children = new Dictionary<String, node>();
+        }
+
     }
 
-    private class file : node {
+    private class file : node
+    {
         public enum format { BASIC, TXT, PRGM, CONF }
         int size;
         object data;
     }
+}
+
+public class CustomException : Exception
+{
+    public CustomException(string message) : base(message) { }
 }
